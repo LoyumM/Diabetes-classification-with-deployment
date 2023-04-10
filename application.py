6b3061fabcc21b851bab2a0019fc3a25a0a86718
LoyumM <lmbmoirangthem033@gmail.com>
2023-04-11 00:14:04 +0530

    updated application

--- application.py
+++ application.py
@@ -43,5 +43,5 @@ def predict_datapoint():
         return render_template('home.html',results=results)
     
 if __name__ == "__main__":
-    # app.run(host = "0.0.0.0", port = 8000)
-    app.run(host = "0.0.0.0", debug = True)
+    app.run(host = "0.0.0.0", port = 8000)
+    # app.run(host = "0.0.0.0", debug = True)
